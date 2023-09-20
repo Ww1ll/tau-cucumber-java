d20d4c7a3eda03ca707261c9518d6a4f3d21e9dd
William <109118303+Ww1ll@users.noreply.github.com>
2023-09-19 23:46:25 -0300

    feat: chapter3.1-final

---
+++ cucumber/src/test/java/com/tau/steps/Steps.java
@@ -0,0 +1,40 @@
+package com.tau.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Steps {
+
+    private WebDriver driver;
+
+    @Given("I am in the login page of the Para Bank Application")
+    public void i_am_in_the_login_page_of_the_para_bank_application() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\vemSER\\TAU\\tau\\tau-cucumber-java\\cucumber\\src\\driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+
+    }
+    @When("I enter valid credentials")
+    public void i_enter_valid_credentials() {
+
+        driver.findElement(By.name("username")).sendKeys("tautester");
+        driver.findElement(By.name("password")).sendKeys("password");
+        driver.findElement(By.name("username")).submit();
+
+    }
+    @Then("I should be taken to the Overview Page")
+    public void i_should_be_taken_to_the_overview_page() {
+
+        //Thread.sleep(5000);
+
+        driver.findElement(By.xpath("//*[@id=\rightPanel\"]/div/div/h1")).isDisplayed();
+        driver.findElement(By.linkText("Log out")).click();
+
+    }
+
+}
---
+++ cucumber/src/test/resources/Login.feature
@@ -0,0 +1,11 @@
+Feature: Login Functionality
+
+  In order to do internet banking
+  As a valid Para Bank customer
+  I want to login succesfully
+
+Scenario: Login Sucessfull
+
+Given I am in the login page of the Para Bank Application
+When I enter valid credentials
+Then I should be taken to the Overview Page
